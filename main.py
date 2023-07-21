ca603144467df920b5e29aea01b2604ac57ae9e5
samsquire <sam@samsquire.com>
2023-07-21 11:21:28 +0000

    Show entire interaction space

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import itertools
 from collections import Counter
 
+
 class Tree:
+
   def __init__(self, name, can_repeat):
     self.name = name
     self.taken = False
     self.children = []
     self.can_repeat = can_repeat
+
   def __repr__(self):
     return self.name
 
@@ -22,7 +25,7 @@ class Tree:
         break
 
     return not found
-  
+
   def order(self, visited, recurse=False):
     if self.taken:
       yield self, "{}".format(self.name)
@@ -36,18 +39,16 @@ class Tree:
           myvisited.append(item.name)
           yield from item.order(myvisited, True)
 
-      
   def walk(self, indent=0, visited=[], recurse=False):
     tag = ""
     if self.can_repeat:
       tag = "(recursive)"
-    
+
     # print("{}{}{}".format(((indent * 2)) * " ", self, tag))
-    
-      
+
     if recurse:
       for child in self.children:
-    
+
         myvisited = list(visited)
         yield (child, "{}{}".format((1 + (indent * 2)) * " ", child))
         if child.name in visited:
@@ -56,13 +57,12 @@ class Tree:
         else:
           myvisited.append(child.name)
           yield from child.walk(indent + 1, myvisited, True)
-          
-        
+
       # visited[self] = True
-        
+
   def add_child(self, name, can_repeat):
     new_node = Tree(name, can_repeat)
-    new_children = []    
+    new_children = []
     for child in self.children:
       new_children.append(Tree(child.name, child.can_repeat))
     new_node.children = new_children
@@ -70,13 +70,9 @@ class Tree:
 
     return new_node
 
-definitions = [
-  ("windows", False),
-  ("mac", False),
-  ("linux", False),
-  ("enter_scope", True),
-  ("exit_scope", True)
-]
+
+definitions = [("windows", False), ("mac", False), ("linux", False),
+               ("enter_scope", True), ("exit_scope", True)]
 
 root = Tree("root", True)
 for event in definitions:
@@ -84,14 +80,9 @@ for event in definitions:
 defs = root.walk(0, {}, True)
 for item in defs:
   print(item[1])
-events = [
-  ("+", "windows", False),
-  ("+", "mac", False),
-  ("+", "linux", False),
-  ("+", "enter_scope", True),
-  ("+", "exit_scope", True),
-  ("+", "windows", True)
-]
+events = [("+", "windows", False), ("+", "mac", False), ("+", "linux", False),
+          ("+", "enter_scope", True), ("+", "exit_scope", True),
+          ("+", "windows", True)]
 gap = Tree("gap", True)
 gaproot = gap
 last_position = gap
@@ -100,26 +91,22 @@ for event in events:
   my_children = list(last_position.childrenwalk())
   print(my_children)
   for item in my_children:
-    
+
     if item[0].name == event[1]:
       print(item[0].name, "is already in the gap")
-      
+
       child = last_position.add_child(item[0].name, item[0].can_repeat)
       child.taken = True
       last_position = child
-      
+
       found = True
       break
   if not found:
     for item in root.childrenwalk():
-      
+
       if item[0].name == event[1]:
         print(item[0].name, "found in root")
-       
-        
-       
 
-        
         child = last_position.add_child(item[0].name, item[0].can_repeat)
         last_position = child
         child.taken = True
@@ -129,8 +116,6 @@ for event in events:
         if last_position.doesnt_contain(item[0].name):
           last_position.add_child(item[0].name, item[0].can_repeat)
 
-    
-  
   print(event)
 
 print("### GAP OUTPUT")
@@ -149,8 +134,10 @@ for item in gaproot.order([], True):
 
 operating_systems = ["windows", "mac", "linux"]
 
-
-print(list(itertools.permutations(["scope_enter", "initialize_variable", "scope_exit"])))
+print(
+    list(
+        itertools.permutations(
+            ["scope_enter", "initialize_variable", "scope_exit"])))
 
 # scope_enter -> scope_exit
 # scope_enter -> initialize_variable ->
@@ -165,7 +152,6 @@ print(list(itertools.permutations(["scope_enter", "initialize_variable", "scope_
 # neurons, dendrites
 
 # token stream
-
 
 # multidimensional AST
 # graph is already in order if it is directed
