a0fc87278235caa75938f223b057c19cfc8afe2d
samsquire <sam@samsquire.com>
2023-07-21 09:11:43 +0000

    Interaction space being created

--- main.py
+++ main.py
@@ -4,10 +4,39 @@ from collections import Counter
 class Tree:
   def __init__(self, name, can_repeat):
     self.name = name
+    self.taken = False
     self.children = []
     self.can_repeat = can_repeat
   def __repr__(self):
     return self.name
+
+  def childrenwalk(self):
+    for item in self.children:
+      yield item, "{}".format(item.name)
+
+  def doesnt_contain(self, name):
+    found = False
+    for child in self.children:
+      if child.name == name:
+        found = True
+        break
+
+    return not found
+  
+  def order(self, visited, recurse=False):
+    if self.taken:
+      yield self, "{}".format(self.name)
+    if recurse:
+      for item in self.children:
+        myvisited = list(visited)
+        if item.name in visited:
+          myvisited.append(item.name)
+          yield from item.order(myvisited, False)
+        else:
+          myvisited.append(item.name)
+          yield from item.order(myvisited, True)
+
+      
   def walk(self, indent=0, visited=[], recurse=False):
     tag = ""
     if self.can_repeat:
@@ -20,39 +49,103 @@ class Tree:
       for child in self.children:
     
         myvisited = list(visited)
-        print("{}{}".format((1 + (indent * 2)) * " ", child))
-        if child in visited:
-          myvisited.append(child)
-          child.walk(indent + 1, myvisited, False)
+        yield (child, "{}{}".format((1 + (indent * 2)) * " ", child))
+        if child.name in visited:
+          myvisited.append(child.name)
+          yield from child.walk(indent + 1, myvisited, False)
         else:
-          myvisited.append(child)
-          child.walk(indent + 1, myvisited, True)
+          myvisited.append(child.name)
+          yield from child.walk(indent + 1, myvisited, True)
           
         
       # visited[self] = True
         
   def add_child(self, name, can_repeat):
     new_node = Tree(name, can_repeat)
-    
-    if self.can_repeat:
-        new_node.children.append(self)
-    elif not self.can_repeat:
-      new_node.children = self.children
+    new_children = []    
+    for child in self.children:
+      new_children.append(Tree(child.name, child.can_repeat))
+    new_node.children = new_children
     self.children.append(new_node)
 
+    return new_node
+
+definitions = [
+  ("windows", False),
+  ("mac", False),
+  ("linux", False),
+  ("enter_scope", True),
+  ("exit_scope", True)
+]
+
+root = Tree("root", True)
+for event in definitions:
+  root.add_child(event[0], event[1])
+defs = root.walk(0, {}, True)
+for item in defs:
+  print(item[1])
 events = [
   ("+", "windows", False),
   ("+", "mac", False),
   ("+", "linux", False),
-  ("+", "enter_scope", True)
+  ("+", "enter_scope", True),
+  ("+", "exit_scope", True),
+  ("+", "windows", True)
 ]
-
-root = Tree("root", True)
+gap = Tree("gap", True)
+gaproot = gap
+last_position = gap
 for event in events:
-  if event[0] == "+":
-    root.add_child(event[1], event[2])
+  found = False
+  my_children = list(last_position.childrenwalk())
+  print(my_children)
+  for item in my_children:
+    
+    if item[0].name == event[1]:
+      print(item[0].name, "is already in the gap")
+      
+      child = last_position.add_child(item[0].name, item[0].can_repeat)
+      child.taken = True
+      last_position = child
+      
+      found = True
+      break
+  if not found:
+    for item in root.childrenwalk():
+      
+      if item[0].name == event[1]:
+        print(item[0].name, "found in root")
+       
+        
+       
 
-root.walk(0, {}, True)
+        
+        child = last_position.add_child(item[0].name, item[0].can_repeat)
+        last_position = child
+        child.taken = True
+        found = True
+        break
+      else:
+        if last_position.doesnt_contain(item[0].name):
+          last_position.add_child(item[0].name, item[0].can_repeat)
+
+    
+  
+  print(event)
+
+print("### GAP OUTPUT")
+
+defs = gap.walk(0, {}, True)
+for item in defs:
+  #if item[0].taken:
+  print(item[1])
+
+print("### TRAVESAL OF INTERACTION SPACE")
+
+for item in gaproot.order([], True):
+  print(item[1])
+
+# interactions between disperate things
 
 operating_systems = ["windows", "mac", "linux"]
 
@@ -80,3 +173,6 @@ print(list(itertools.permutations(["scope_enter", "initialize_variable", "scope_
 # create a context
 # mashup generator
 # CRDT + megatree + smol_world
+# merge behaviour
+
+# orderings
